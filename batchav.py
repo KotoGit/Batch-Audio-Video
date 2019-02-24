a10205d7e917d7b3d80ee7c3374180aaa2a20644
KotoGit <gsreceiver@gmail.com>
2019-02-24 16:44:09 -0500

    Added input file support

--- batchav.py
+++ batchav.py
@@ -1,4 +1,4 @@
-
+import sys
 import subprocess
 
 class timestamp:
@@ -7,33 +7,55 @@ class timestamp:
         self.end = end
 
 testing = []
-testing.append(timestamp("00:00:00", "00:00:27"))
+with open(sys.argv[1], 'r') as file:
+    contents = [line.rstrip('\n') for line in file]
+    len_contents = len(contents)
+    i = 0
+    while i < len_contents:
+        readtime = contents[i].split('-')
+        testing.append(timestamp(readtime[0], readtime[1]))
+        i += 1
 
-start = input("Do you want the ouput to be audio (a) or video (v)? ")
+filename = sys.argv[2]
+cont = 0
+while cont == 0:
+    start = input("Please choose output format audio (a) or video (v): ")
+    if start == 'a' or start == 'v':
+        cont = 1
 loop = 0
 cmdlist = []
+outname = []
 while loop == 0:
     if start == 'a':
-        filename = input("Print the video filename with extension: ")
-        outname = input("Print the output file name with mp3 extension: ")
+        len_test = len(testing)
+        j = 0
+        while j < len_test:
+            temp = "out" + str(j) + ".mp3"
+            outname.append(temp)
+            j += 1
         program = "ffmpeg"
-        for time in testing:
+        for counter, time in enumerate(testing):
             start = time.begin
             fin = time.end
-            arg = ["-i", filename, "-ss", start, "-to", fin, "-f", "mp3", "-ab", "320000", "-vn", outname]
+            arg = ["-i", filename, "-ss", start, "-to", fin, "-f", "mp3", "-ab", "320000", "-vn", outname[counter]]
             cmdlist.append(arg)
         for idx, command in enumerate(cmdlist):
             temp = subprocess.Popen([program] + cmdlist[idx], stdout=subprocess.PIPE)
             temp.wait()
         loop = 1
     elif start == 'v':
-        filename = input("Print the video filename with extension: ")
-        outname = input("Print the output file name with extension: ")
+        len_test = len(testing)
+        j = 0
+        outext = input("Print the desired output extension: ")
+        while j < len_test:
+            temp = "out" + str(j) + outext
+            outname.append(temp)
+            j += 1
         program = "ffmpeg"
-        for time in testing:
+        for counter, time in enumerate(testing):
             start = time.begin
             fin = time.end
-            arg = ["-i", filename, "-ss", start, "-strict", "-2", "-to", fin, outname]
+            arg = ["-i", filename, "-ss", start, "-strict", "-2", "-to", fin, outname[counter]]
             cmdlist.append(arg)
         for idx, command in enumerate(cmdlist):
             temp = subprocess.Popen([program] + cmdlist[idx], stdout=subprocess.PIPE)
